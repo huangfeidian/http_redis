39cf0de23ef886afda52f6b0d42e85b501e695cf
huang huiliang <spiritsaway@outlook.com>
2022-08-31 23:44:36 +0800

    增加对json字符串的支持

--- include/command/cmd_utils.h
+++ include/command/cmd_utils.h
@@ -2,10 +2,8 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <nlohmann/json.hpp>
 
 namespace spiritsaway::http_redis::command
 {
 	std::string cmd_join(const std::vector<std::string>& fragments);
-	std::string to_redis_str(const nlohmann::json& input);
 }
